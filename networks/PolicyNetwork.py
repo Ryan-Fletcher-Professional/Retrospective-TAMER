7a5d66119c7ed7b96ff64302096b773ec9cf1afe
NephilimGreen <ryan.fletcher.9999@gmail.com>
2024-03-29 14:31:30 -0400

    Initial Commit

---
+++ networks/PolicyNetwork.py
@@ -0,0 +1,7 @@
+import Network
+
+
+class PolicyNetwork(Network):
+    def __init__(self):
+        super().__init__()
+        # TODO define network architecture.
---
+++ networks/RewardNetwork.py
@@ -0,0 +1,7 @@
+import Network
+
+
+class RewardNetwork(Network):
+    def __init__(self):
+        super().__init__()
+        # TODO define network architecture.
