069924331161a60a5ca9fd15a2f46ae4792a9142
hasgrig <hasgrigorian@gmail.com>
2024-04-05 14:34:37 -0400

    mountain car works but does not have gamma function for feedback and does not save network after each run

--- main.py
+++ main.py
@@ -8,56 +8,66 @@ import numpy as np
 import torch.nn.functional as F
 from pynput import keyboard
 import snake_gym
-from main.GLOBALS import * 
-
+from main.GLOBALS import *
+from networks.RewardNetwork import *
 
 device = torch.device('cpu')
 
 
-class PolicyNetwork(nn.Module):
-    '''
-        Simple neural network with two layers that maps a 2-d state to a prediction
-        over which of the three discrete actions should be taken.
-        The three outputs corresponding to the logits for a 3-way classification problem.
+def train_network(net, input, output):
+    optimizer = Adam(net.parameters(), lr=0.2)
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer.zero_grad()
+    #run action,state through policy to get predicted logits for classifying action
+    pred_action_logits = net(torch.as_tensor(input, dtype=torch.float32))
 
-    '''
-    def __init__(self):
-        super().__init__()
-
-        #This layer has 2 inputs corresponding to car position and velocity
-        self.fc1 = nn.Linear(2, 8)
-        #This layer has three outputs corresponding to each of the three discrete actions
-        self.fc2 = nn.Linear(8, 3)
-
-    def forward(self, x):
-        #this method performs a forward pass through the network, applying a non-linearity (ReLU) on the
-        #outputs of the first layer
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
+    # one hot encode output and make it tensor
+    output_one_hot = np.zeros(2)
+    output_one_hot[output]=1
+    output_tensor = torch.as_tensor(output_one_hot)
+    #now compute loss
+    loss = loss_criterion(pred_action_logits, output_tensor)
+    #back propagate the error through the network
+    loss.backward()
+    #perform update on policy parameters
+    optimizer.step()
+    print("Updating network: input ", input, " output: ", output_one_hot)
 
 # currently only works with mountain car
-def collect_live_data(pi, env_name, human_render=True):
+def collect_live_data(net, env_name, human_render=True):
     '''
     Run a live simulation and collect keyboard data
     inputs: policy, gym environment name, and whether to render
     outputs: a list of dictionaries {"state": np.array, "feedback": binary}
     '''
     feedback_data = []
+    obs_data = []
+
     # called when a key is pressed
     def on_press(key):
         # c for negative feedback
         if key.char=='c':
-            feedback_data.append({"state": obs, "feedback": 0})
+            feedback=0
         # v for positive feedback
         if key.char=='v':
-            feedback_data.append({"obs": obs, "feedback": 1})
+            feedback=1
+
+        # one hot encode the actions
+        one_hot_action = np.zeros(3)
+        one_hot_action[action]=1
+
+        # append the state, actions to obs_data
+        state_action = np.append(obs, one_hot_action)
+        obs_data.append(state_action)
+        feedback_data.append(feedback)
+        train_network(net, state_action, feedback)
+
 
     # start a keyboard listener
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
 
+    # whether to render the game in a window or not
     if human_render:
         env = gym.make(env_name, render_mode='human')
     else:
@@ -72,7 +82,8 @@ def collect_live_data(pi, env_name, human_render=True):
         # take the action that the network assigns the highest logit value to
         # Note that first we convert from numpy to tensor and then we get the value of the
         # argmax using .item() and feed that into the environment
-        action = torch.argmax(pi(torch.from_numpy(obs).unsqueeze(0))).item()
+        #action = torch.argmax(pi(torch.from_numpy(obs).unsqueeze(0))).item()
+        action = net.predict_max_action(obs)
         #print(action)
         obs, rew, terminated, truncated, info = env.step(action)
         done = terminated or truncated
@@ -81,7 +92,9 @@ def collect_live_data(pi, env_name, human_render=True):
     # stop the keyboard listener
     listener.stop()
     listener.join()
-    return feedback_data
+
+    output_dict = {"states":np.asarray(obs_data), "feedback":np.asarray(feedback_data)}
+    return output_dict
 
 
 if __name__ == "__main__":
@@ -90,10 +103,11 @@ if __name__ == "__main__":
     #parser.add_argument('--num_bc_iters', default = 100, type=int, help="number of iterations to run BC")
     #parser.add_argument('--num_inv_dyn_iters', default = 500, type=int, help="number of iterations to train inverse dynamics model")
     #parser.add_argument('--num_evals', default=6, type=int, help="number of times to run policy after training for evaluation")
-
     #args = parser.parse_args()
-    pi = PolicyNetwork()
-    data = collect_live_data(pi, env_name="MountainCar-v0")
+
+    
+    net = CarRewardNet()
+    data = collect_live_data(net, env_name="MountainCar-v0")
     print(data)
 
     #collect human demos
--- networks/RewardNetwork.py
+++ networks/RewardNetwork.py
@@ -1,10 +1,9 @@
-import Network
+import torch
 import torch.nn as nn
-
-
-class RewardNetwork(Network):
-    def __init__(self, sizes=None):
-        super().__init__(sizes)
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+from torch.optim import Adam
+import numpy as np
 
 
 '''
@@ -17,9 +16,9 @@ class CarRewardNet(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(2, 16)
+        self.fc1 = nn.Linear(5, 16)
         self.fc2 = nn.Linear(16, 16)
-        self.fc3 = nn.Linear(16, 3)
+        self.fc3 = nn.Linear(16, 2)
 
     def forward(self, x):
         x = F.leaky_relu(self.fc1(x))
@@ -27,6 +26,25 @@ class CarRewardNet(nn.Module):
         x = self.fc3(x)
         return x
 
-    def predict_return(self, traj):
-        out = self(traj)
+    def predict(self, state_action):
+        out = self(state_action)
         return out
+
+    def predict_max_action(self, state):
+        # creates a matrix [1, 0, 0,
+        #                   0, 1, 0,
+        #                   0, 0, 1]
+
+        action_space = np.eye(3)
+        max_action_ind = 0
+        preds = []
+        print("----------------------")
+        for action in action_space:
+            state_action = torch.as_tensor(np.append(state, action), dtype=torch.float32)
+            new_pred = self.predict(state_action)
+            new_pred_proba = nn.functional.softmax(new_pred)
+            preds.append(new_pred_proba[1].item())
+            print(new_pred_proba)
+        best_action_ind = np.argmax(np.asarray(preds))
+        print("best action", best_action_ind)
+        return best_action_ind
