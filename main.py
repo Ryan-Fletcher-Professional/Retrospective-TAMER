9a8d52003338b9dd36196cb6f241e6d969aa17b2
NephilimGreen <ryan.fletcher.9999@gmail.com>
2024-04-13 21:23:19 -0400

    Merged current code together

--- main.py
+++ main.py
@@ -32,18 +32,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('--num_plays', default=5, type=int, help="number of play runs to collect")
     parser.add_argument('--mode', default=DEFAULT_MODE, type=str, help="Which game to do")
-    parser.add_argument('--snake_max_fps', default=10, type=int, help="max fps at which snake game should run")
+    parser.add_argument('--fps', default=10, type=int, help="max fps at which snake game should run")
     parser.add_argument('--frame_limit', default=200, type=int, help="number of frames before episode cuts off")
     parser.add_argument('--feedback_mode', default="online", type=str, help="online/offline : how feedback will be given?")
-    #parser.add_argument('--num_evals', default=6, type=int, help="number of times to run policy after training for evaluation")
     args = parser.parse_args()
 
     net = Net(args.mode)
     for i in range(args.num_plays):
         if args.feedback_mode=="online":
-            data = online.collect_live_data(net, env_name=args.mode, frame_limit=args.frame_limit, snake_max_fps=args.snake_max_fps)
+            data = online.collect_live_data(net, env_name=args.mode, frame_limit=args.frame_limit, snake_max_fps=args.fps)
             print("Data for run " + str(i + 1) + ":\n" + str(data))
         elif args.feedback_mode=="offline":
-            offline_wrapper(net, env_name=args.mode, frame_limit=args.frame_limit, snake_max_fps=args.snake_max_fps)
+            offline_wrapper(net, env_name=args.mode, frame_limit=args.frame_limit, snake_max_fps=args.fps)
             print("Didn't collect data, but offline run " + str(i + 1) + " complete.")
         
